8cc09fa829f6498ed6366e5e29b26f880495790d
Ashrafulaziz9991 <ashrafulaziz9991@email.com>
2025-10-08 19:56:28 +0600

    hi

---
+++ week 1 : Introduction to python/module 1 : Introduction to python/data_type&variable.py
@@ -0,0 +1,37 @@
+""" Basic data type & Variable details : """
+
+print("Hi everyone")
+# int type data
+num = 12
+
+# string type data
+name = "Akkas"
+
+area = "Kuratoli"
+
+# string type data
+address = "Kuratoli, Dhaka - 1229"
+
+# string type data using single quote
+Region = 'Dhaka'
+
+# charecter type data
+section = "D"
+
+# float type data
+fracton = 2.8
+
+# boolean data type
+is_Devoloped = False
+
+# to check those variable & thier data types we'll use print function 
+
+print(num, "data type is ", type(num))
+print(name, "data type is ", type(name))
+print(address, "data type is ", type(address))
+print(Region, "data type is ", type(Region))
+print(section, "data type is ", type(section))
+print(fracton, "data type is ", type(fracton))
+print(is_Devoloped, "data type is ", type(is_Devoloped))
+
+print(area ," ", Region)
