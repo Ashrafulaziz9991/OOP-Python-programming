c6f78633d4d6fda1b16d614ef2e21694404c3829
Ashrafulaziz9991 <ashrafulaziz9991@gmail.com>
2026-09-10 00:36:51 +0600

    added operator files with operations

---
+++ week 1 : Introduction to python/operator.py
@@ -0,0 +1,21 @@
+"""
+# We'll learn about 
+- uses of '+', '-', '*', '/', '//', '%'
+"""
+
+a = 6
+b = 4
+
+'''Direct operation'''
+
+print("Sum is", a + b)
+print("Subtract is", a - b)
+print("Multiply is", a * b)
+
+# into Float format
+print("Division is ", a / b)
+# into Integer format
+print("Division is", a // b)
+
+print("Reminder is", a % b)
+
